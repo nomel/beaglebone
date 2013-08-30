7f8dcf465576d64c93bd53a2b8f5a644dc2c7eb7
Nomel <nomel@nomel.org>
2013-08-30 08:20:49 +0000

    Added generated files for the lazy

---
+++ gpio-header/generated/compile.sh
@@ -0,0 +1,128 @@
+echo Compiling dts files...
+echo \ \ gpio-P8.3-00A0.dts -\> gpio-P8.3-00A0.dtbo
+dtc -O dtb -o gpio-P8.3-00A0.dtbo -b 0 -@ -I dts gpio-P8.3-00A0.dts
+echo \ \ gpio-P8.4-00A0.dts -\> gpio-P8.4-00A0.dtbo
+dtc -O dtb -o gpio-P8.4-00A0.dtbo -b 0 -@ -I dts gpio-P8.4-00A0.dts
+echo \ \ gpio-P8.5-00A0.dts -\> gpio-P8.5-00A0.dtbo
+dtc -O dtb -o gpio-P8.5-00A0.dtbo -b 0 -@ -I dts gpio-P8.5-00A0.dts
+echo \ \ gpio-P8.6-00A0.dts -\> gpio-P8.6-00A0.dtbo
+dtc -O dtb -o gpio-P8.6-00A0.dtbo -b 0 -@ -I dts gpio-P8.6-00A0.dts
+echo \ \ gpio-P8.7-00A0.dts -\> gpio-P8.7-00A0.dtbo
+dtc -O dtb -o gpio-P8.7-00A0.dtbo -b 0 -@ -I dts gpio-P8.7-00A0.dts
+echo \ \ gpio-P8.8-00A0.dts -\> gpio-P8.8-00A0.dtbo
+dtc -O dtb -o gpio-P8.8-00A0.dtbo -b 0 -@ -I dts gpio-P8.8-00A0.dts
+echo \ \ gpio-P8.9-00A0.dts -\> gpio-P8.9-00A0.dtbo
+dtc -O dtb -o gpio-P8.9-00A0.dtbo -b 0 -@ -I dts gpio-P8.9-00A0.dts
+echo \ \ gpio-P8.10-00A0.dts -\> gpio-P8.10-00A0.dtbo
+dtc -O dtb -o gpio-P8.10-00A0.dtbo -b 0 -@ -I dts gpio-P8.10-00A0.dts
+echo \ \ gpio-P8.11-00A0.dts -\> gpio-P8.11-00A0.dtbo
+dtc -O dtb -o gpio-P8.11-00A0.dtbo -b 0 -@ -I dts gpio-P8.11-00A0.dts
+echo \ \ gpio-P8.12-00A0.dts -\> gpio-P8.12-00A0.dtbo
+dtc -O dtb -o gpio-P8.12-00A0.dtbo -b 0 -@ -I dts gpio-P8.12-00A0.dts
+echo \ \ gpio-P8.13-00A0.dts -\> gpio-P8.13-00A0.dtbo
+dtc -O dtb -o gpio-P8.13-00A0.dtbo -b 0 -@ -I dts gpio-P8.13-00A0.dts
+echo \ \ gpio-P8.14-00A0.dts -\> gpio-P8.14-00A0.dtbo
+dtc -O dtb -o gpio-P8.14-00A0.dtbo -b 0 -@ -I dts gpio-P8.14-00A0.dts
+echo \ \ gpio-P8.15-00A0.dts -\> gpio-P8.15-00A0.dtbo
+dtc -O dtb -o gpio-P8.15-00A0.dtbo -b 0 -@ -I dts gpio-P8.15-00A0.dts
+echo \ \ gpio-P8.16-00A0.dts -\> gpio-P8.16-00A0.dtbo
+dtc -O dtb -o gpio-P8.16-00A0.dtbo -b 0 -@ -I dts gpio-P8.16-00A0.dts
+echo \ \ gpio-P8.17-00A0.dts -\> gpio-P8.17-00A0.dtbo
+dtc -O dtb -o gpio-P8.17-00A0.dtbo -b 0 -@ -I dts gpio-P8.17-00A0.dts
+echo \ \ gpio-P8.18-00A0.dts -\> gpio-P8.18-00A0.dtbo
+dtc -O dtb -o gpio-P8.18-00A0.dtbo -b 0 -@ -I dts gpio-P8.18-00A0.dts
+echo \ \ gpio-P8.19-00A0.dts -\> gpio-P8.19-00A0.dtbo
+dtc -O dtb -o gpio-P8.19-00A0.dtbo -b 0 -@ -I dts gpio-P8.19-00A0.dts
+echo \ \ gpio-P8.20-00A0.dts -\> gpio-P8.20-00A0.dtbo
+dtc -O dtb -o gpio-P8.20-00A0.dtbo -b 0 -@ -I dts gpio-P8.20-00A0.dts
+echo \ \ gpio-P8.21-00A0.dts -\> gpio-P8.21-00A0.dtbo
+dtc -O dtb -o gpio-P8.21-00A0.dtbo -b 0 -@ -I dts gpio-P8.21-00A0.dts
+echo \ \ gpio-P8.22-00A0.dts -\> gpio-P8.22-00A0.dtbo
+dtc -O dtb -o gpio-P8.22-00A0.dtbo -b 0 -@ -I dts gpio-P8.22-00A0.dts
+echo \ \ gpio-P8.23-00A0.dts -\> gpio-P8.23-00A0.dtbo
+dtc -O dtb -o gpio-P8.23-00A0.dtbo -b 0 -@ -I dts gpio-P8.23-00A0.dts
+echo \ \ gpio-P8.24-00A0.dts -\> gpio-P8.24-00A0.dtbo
+dtc -O dtb -o gpio-P8.24-00A0.dtbo -b 0 -@ -I dts gpio-P8.24-00A0.dts
+echo \ \ gpio-P8.25-00A0.dts -\> gpio-P8.25-00A0.dtbo
+dtc -O dtb -o gpio-P8.25-00A0.dtbo -b 0 -@ -I dts gpio-P8.25-00A0.dts
+echo \ \ gpio-P8.26-00A0.dts -\> gpio-P8.26-00A0.dtbo
+dtc -O dtb -o gpio-P8.26-00A0.dtbo -b 0 -@ -I dts gpio-P8.26-00A0.dts
+echo \ \ gpio-P8.27-00A0.dts -\> gpio-P8.27-00A0.dtbo
+dtc -O dtb -o gpio-P8.27-00A0.dtbo -b 0 -@ -I dts gpio-P8.27-00A0.dts
+echo \ \ gpio-P8.28-00A0.dts -\> gpio-P8.28-00A0.dtbo
+dtc -O dtb -o gpio-P8.28-00A0.dtbo -b 0 -@ -I dts gpio-P8.28-00A0.dts
+echo \ \ gpio-P8.29-00A0.dts -\> gpio-P8.29-00A0.dtbo
+dtc -O dtb -o gpio-P8.29-00A0.dtbo -b 0 -@ -I dts gpio-P8.29-00A0.dts
+echo \ \ gpio-P8.30-00A0.dts -\> gpio-P8.30-00A0.dtbo
+dtc -O dtb -o gpio-P8.30-00A0.dtbo -b 0 -@ -I dts gpio-P8.30-00A0.dts
+echo \ \ gpio-P8.31-00A0.dts -\> gpio-P8.31-00A0.dtbo
+dtc -O dtb -o gpio-P8.31-00A0.dtbo -b 0 -@ -I dts gpio-P8.31-00A0.dts
+echo \ \ gpio-P8.32-00A0.dts -\> gpio-P8.32-00A0.dtbo
+dtc -O dtb -o gpio-P8.32-00A0.dtbo -b 0 -@ -I dts gpio-P8.32-00A0.dts
+echo \ \ gpio-P8.33-00A0.dts -\> gpio-P8.33-00A0.dtbo
+dtc -O dtb -o gpio-P8.33-00A0.dtbo -b 0 -@ -I dts gpio-P8.33-00A0.dts
+echo \ \ gpio-P8.34-00A0.dts -\> gpio-P8.34-00A0.dtbo
+dtc -O dtb -o gpio-P8.34-00A0.dtbo -b 0 -@ -I dts gpio-P8.34-00A0.dts
+echo \ \ gpio-P8.35-00A0.dts -\> gpio-P8.35-00A0.dtbo
+dtc -O dtb -o gpio-P8.35-00A0.dtbo -b 0 -@ -I dts gpio-P8.35-00A0.dts
+echo \ \ gpio-P8.36-00A0.dts -\> gpio-P8.36-00A0.dtbo
+dtc -O dtb -o gpio-P8.36-00A0.dtbo -b 0 -@ -I dts gpio-P8.36-00A0.dts
+echo \ \ gpio-P8.37-00A0.dts -\> gpio-P8.37-00A0.dtbo
+dtc -O dtb -o gpio-P8.37-00A0.dtbo -b 0 -@ -I dts gpio-P8.37-00A0.dts
+echo \ \ gpio-P8.38-00A0.dts -\> gpio-P8.38-00A0.dtbo
+dtc -O dtb -o gpio-P8.38-00A0.dtbo -b 0 -@ -I dts gpio-P8.38-00A0.dts
+echo \ \ gpio-P8.39-00A0.dts -\> gpio-P8.39-00A0.dtbo
+dtc -O dtb -o gpio-P8.39-00A0.dtbo -b 0 -@ -I dts gpio-P8.39-00A0.dts
+echo \ \ gpio-P8.40-00A0.dts -\> gpio-P8.40-00A0.dtbo
+dtc -O dtb -o gpio-P8.40-00A0.dtbo -b 0 -@ -I dts gpio-P8.40-00A0.dts
+echo \ \ gpio-P8.41-00A0.dts -\> gpio-P8.41-00A0.dtbo
+dtc -O dtb -o gpio-P8.41-00A0.dtbo -b 0 -@ -I dts gpio-P8.41-00A0.dts
+echo \ \ gpio-P8.42-00A0.dts -\> gpio-P8.42-00A0.dtbo
+dtc -O dtb -o gpio-P8.42-00A0.dtbo -b 0 -@ -I dts gpio-P8.42-00A0.dts
+echo \ \ gpio-P8.43-00A0.dts -\> gpio-P8.43-00A0.dtbo
+dtc -O dtb -o gpio-P8.43-00A0.dtbo -b 0 -@ -I dts gpio-P8.43-00A0.dts
+echo \ \ gpio-P8.44-00A0.dts -\> gpio-P8.44-00A0.dtbo
+dtc -O dtb -o gpio-P8.44-00A0.dtbo -b 0 -@ -I dts gpio-P8.44-00A0.dts
+echo \ \ gpio-P8.45-00A0.dts -\> gpio-P8.45-00A0.dtbo
+dtc -O dtb -o gpio-P8.45-00A0.dtbo -b 0 -@ -I dts gpio-P8.45-00A0.dts
+echo \ \ gpio-P8.46-00A0.dts -\> gpio-P8.46-00A0.dtbo
+dtc -O dtb -o gpio-P8.46-00A0.dtbo -b 0 -@ -I dts gpio-P8.46-00A0.dts
+echo \ \ gpio-P9.11-00A0.dts -\> gpio-P9.11-00A0.dtbo
+dtc -O dtb -o gpio-P9.11-00A0.dtbo -b 0 -@ -I dts gpio-P9.11-00A0.dts
+echo \ \ gpio-P9.12-00A0.dts -\> gpio-P9.12-00A0.dtbo
+dtc -O dtb -o gpio-P9.12-00A0.dtbo -b 0 -@ -I dts gpio-P9.12-00A0.dts
+echo \ \ gpio-P9.13-00A0.dts -\> gpio-P9.13-00A0.dtbo
+dtc -O dtb -o gpio-P9.13-00A0.dtbo -b 0 -@ -I dts gpio-P9.13-00A0.dts
+echo \ \ gpio-P9.15-00A0.dts -\> gpio-P9.15-00A0.dtbo
+dtc -O dtb -o gpio-P9.15-00A0.dtbo -b 0 -@ -I dts gpio-P9.15-00A0.dts
+echo \ \ gpio-P9.17-00A0.dts -\> gpio-P9.17-00A0.dtbo
+dtc -O dtb -o gpio-P9.17-00A0.dtbo -b 0 -@ -I dts gpio-P9.17-00A0.dts
+echo \ \ gpio-P9.18-00A0.dts -\> gpio-P9.18-00A0.dtbo
+dtc -O dtb -o gpio-P9.18-00A0.dtbo -b 0 -@ -I dts gpio-P9.18-00A0.dts
+echo \ \ gpio-P9.19-00A0.dts -\> gpio-P9.19-00A0.dtbo
+dtc -O dtb -o gpio-P9.19-00A0.dtbo -b 0 -@ -I dts gpio-P9.19-00A0.dts
+echo \ \ gpio-P9.20-00A0.dts -\> gpio-P9.20-00A0.dtbo
+dtc -O dtb -o gpio-P9.20-00A0.dtbo -b 0 -@ -I dts gpio-P9.20-00A0.dts
+echo \ \ gpio-P9.21-00A0.dts -\> gpio-P9.21-00A0.dtbo
+dtc -O dtb -o gpio-P9.21-00A0.dtbo -b 0 -@ -I dts gpio-P9.21-00A0.dts
+echo \ \ gpio-P9.22-00A0.dts -\> gpio-P9.22-00A0.dtbo
+dtc -O dtb -o gpio-P9.22-00A0.dtbo -b 0 -@ -I dts gpio-P9.22-00A0.dts
+echo \ \ gpio-P9.23-00A0.dts -\> gpio-P9.23-00A0.dtbo
+dtc -O dtb -o gpio-P9.23-00A0.dtbo -b 0 -@ -I dts gpio-P9.23-00A0.dts
+echo \ \ gpio-P9.24-00A0.dts -\> gpio-P9.24-00A0.dtbo
+dtc -O dtb -o gpio-P9.24-00A0.dtbo -b 0 -@ -I dts gpio-P9.24-00A0.dts
+echo \ \ gpio-P9.25-00A0.dts -\> gpio-P9.25-00A0.dtbo
+dtc -O dtb -o gpio-P9.25-00A0.dtbo -b 0 -@ -I dts gpio-P9.25-00A0.dts
+echo \ \ gpio-P9.26-00A0.dts -\> gpio-P9.26-00A0.dtbo
+dtc -O dtb -o gpio-P9.26-00A0.dtbo -b 0 -@ -I dts gpio-P9.26-00A0.dts
+echo \ \ gpio-P9.27-00A0.dts -\> gpio-P9.27-00A0.dtbo
+dtc -O dtb -o gpio-P9.27-00A0.dtbo -b 0 -@ -I dts gpio-P9.27-00A0.dts
+echo \ \ gpio-P9.28-00A0.dts -\> gpio-P9.28-00A0.dtbo
+dtc -O dtb -o gpio-P9.28-00A0.dtbo -b 0 -@ -I dts gpio-P9.28-00A0.dts
+echo \ \ gpio-P9.29-00A0.dts -\> gpio-P9.29-00A0.dtbo
+dtc -O dtb -o gpio-P9.29-00A0.dtbo -b 0 -@ -I dts gpio-P9.29-00A0.dts
+echo \ \ gpio-P9.30-00A0.dts -\> gpio-P9.30-00A0.dtbo
+dtc -O dtb -o gpio-P9.30-00A0.dtbo -b 0 -@ -I dts gpio-P9.30-00A0.dts
+echo \ \ gpio-P9.31-00A0.dts -\> gpio-P9.31-00A0.dtbo
+dtc -O dtb -o gpio-P9.31-00A0.dtbo -b 0 -@ -I dts gpio-P9.31-00A0.dts
+echo Done.
